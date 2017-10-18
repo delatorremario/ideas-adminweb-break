8aba3c683e1cdb3b0f0c73a92d8f8db7d3014287
Mario de la Torret <delatorremario@gmail.com>
2017-10-18 16:54:52 -0300

    add nginx confix and fix starts

--- server/main.js
+++ server/main.js
@@ -1,20 +1,6 @@
-import { Meteor } from 'meteor/meteor'
-import './start'
+import { Meteor } from 'meteor/meteor';
+import Start from './start';
 
 Meteor.startup(() => {
-
-    if (!Meteor.users.findOne({ 'emails.address': 'mariodelatorre@holos.cl' }))
-        Accounts.createUser({ email: 'mariodelatorre@holos.cl', password: 'Holos123' })
-
-    if (!Meteor.users.findOne({ 'emails.address': 'cbaiardi@holos.cl' }))
-        Accounts.createUser({ email: 'cbaiardi@holos.cl', password: 'Holos123' })
-
-    Start.start()
-
-    // console.log(Roles.getAllRoles())
-
-    // 1) code to run on server at startup
-    // 2) preguntar por el suuario cbaiardi@holos.cl, mariodelatorre@holos.cl si no crearlos
-    // 3) preguntar por el rol superadmin , si no existe... crearlo
-    // 4) preguntar si el correo cbaiardi@holos.cl, mariodelatorre@holos.cl esta en ese rol... sino agregarlo
+  Start.start();
 });
--- server/start.js
+++ server/start.js
@@ -1,38 +1,44 @@
-export default Start = {
-    superAdminHolos: false,
-    adminGrupoNegocio: false,
-    start: () => {
-        Start.initRoles()
-        Start.addMeToAdmin()
-    },
-    initRoles: () => {
-        var roles = Roles.getAllRoles()
-        roles.map(role => {
-            if (role.name == 'SuperAdminHolos')
-                Start.superAdminHolos = true
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+import { Accounts } from 'meteor/accounts-base';
+import _ from 'lodash';
 
-            if (role.name == 'AdminGrupoNegocio')
-                Start.adminGrupoNegocio = true
-        });
+const createAccounts = () => {
+  if (!Meteor.users.findOne({ 'emails.address': 'mariodelatorre@holos.cl' }))
+    Accounts.createUser({ email: 'mariodelatorre@holos.cl', password: 'Holos123' });
 
-        if (!Start.superAdminHolos)
-            Roles.createRole('SuperAdminHolos')
+  if (!Meteor.users.findOne({ 'emails.address': 'cbaiardi@holos.cl' }))
+    Accounts.createUser({ email: 'cbaiardi@holos.cl', password: 'Holos123' });
 
-        if (!Start.adminGrupoNegocio)
-            Roles.createRole('AdminGrupoNegocio')
+  if (!Meteor.users.findOne({ 'emails.address': 'ctomba@holos.cl' }))
+    Accounts.createUser({ email: 'ctomba@holos.cl', password: 'Holos123' });
 
-    },
-    addMeToAdmin: () => {
-        if (Meteor.users.find({ 'emails.address': 'mariodelatorre@holos.cl' }).count() > 0) {
-            var usr = Meteor.users.findOne({ 'emails.address': 'mariodelatorre@holos.cl' })
-            Roles.addUsersToRoles(usr, ['SuperAdminHolos'])
-            console.log('user agreado a SuperAdminHolos',usr)
-        }
+  if (!Meteor.users.findOne({ 'emails.address': 'dblazina@holos.cl' }))
+    Accounts.createUser({ email: 'dblazina@holos.cl', password: 'HolosBlazina' });
+};
 
-        if (Meteor.users.find({ 'emails.address': 'cbaiardi@holos.cl' }).count() > 0) {
-            var usr = Meteor.users.findOne({ 'emails.address': 'cbaiardi@holos.cl' })
-            Roles.addUsersToRoles(usr, ['SuperAdminHolos'])
-            console.log('user agreado a SuperAdminHolos',usr)
-        }
+const initRoles = () => {
+  if (_.includes(Roles.getAllRoles(), 'SuperAdminHolos')) Roles.createRole('SuperAdminHolos');
+  if (_.includes(Roles.getAllRoles(), 'AdminGrupoNegocio')) Roles.createRole('AdminGrupoNegocio');
+};
+
+const addMeToAdmin = () => {
+  const mails = ['mariodelatorre@holos.cl', 'ctomba@holos.cl', 'cbaiardi@holos.cl', 'dblazina@holos.cl'];
+
+  _.map(mails, (mail) => {
+    if (Meteor.users.find({ 'emails.address': mail }).count() > 0 && !Roles.userIsInRole(Meteor.users.find({ 'emails.address': mail }), ['SuperAdminHolos'])) {
+      Roles.addUsersToRoles(Meteor.users.findOne({ 'emails.address': mail }), ['SuperAdminHolos']);
+      console.log('user agreado a SuperAdminHolos', mail);
     }
-}
+  });
+};
+
+const Start = {
+  start: () => {
+    createAccounts();
+    initRoles();
+    addMeToAdmin();
+  },
+};
+
+export default Start;
