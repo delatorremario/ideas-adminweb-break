f975fd5c3d27ceb8f591ae22a5687263416657cf
Mario de la Torret <delatorremario@gmail.com>
2017-10-18 17:05:25 -0300

    fix start

--- server/start.js
+++ server/start.js
@@ -4,17 +4,13 @@ import { Accounts } from 'meteor/accounts-base';
 import _ from 'lodash';
 
 const createAccounts = () => {
-  if (!Meteor.users.findOne({ 'emails.address': 'mariodelatorre@holos.cl' }))
-    Accounts.createUser({ email: 'mariodelatorre@holos.cl', password: 'Holos123' });
 
-  if (!Meteor.users.findOne({ 'emails.address': 'cbaiardi@holos.cl' }))
-    Accounts.createUser({ email: 'cbaiardi@holos.cl', password: 'Holos123' });
+  const mails = ['mariodelatorre@holos.cl', 'ctomba@holos.cl', 'cbaiardi@holos.cl', 'dblazina@holos.cl'];
 
-  if (!Meteor.users.findOne({ 'emails.address': 'ctomba@holos.cl' }))
-    Accounts.createUser({ email: 'ctomba@holos.cl', password: 'Holos123' });
-
-  if (!Meteor.users.findOne({ 'emails.address': 'dblazina@holos.cl' }))
-    Accounts.createUser({ email: 'dblazina@holos.cl', password: 'HolosBlazina' });
+  _.map(mails, (mail) => {
+    if (!Meteor.users.findOne({ 'emails.address': mail }))
+      Accounts.createUser({ email: mail, password: 'Holos123' });
+  });
 };
 
 const initRoles = () => {
