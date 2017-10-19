2f84d71a5a83e9a8bef76cc3c818d59fdace9cd5
Mario de la Torret <delatorremario@gmail.com>
2017-10-19 17:35:22 -0300

    add routes recovry pass

--- imports/ui/layouts/App.js
+++ imports/ui/layouts/App.js
@@ -1,11 +1,16 @@
 import React, { PropTypes } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, browserHistory } from 'react-router-dom';
 import { composeWithTracker } from 'react-komposer';
 import { Meteor } from 'meteor/meteor';
 import Public from '../pages/Public';
 import Authenticated from '../pages/Authenticated';
+
 // import Index from '../pages/Index';
+
 import Login from '../pages/Login';
+import RecoverPassword from '../pages/RecoverPassword';
+import ResetPassword from '../pages/ResetPassword';
+
 // Layouts components pages
 
 import MainWrapper from './MainWrapper';
@@ -14,8 +19,13 @@ const App = appProps => (
   <Router>
     <div>
        {/* <Route exact path="/" component={Index} /> */}
-      <Public path="/login" component={Login} {...appProps} />
-      <Authenticated path="/" component={MainWrapper} {...appProps} />
+       <Route exact path="/" component={Login} {...appProps} />
+       
+       <Route name="recover-password" path="/recover-password" component={RecoverPassword} />
+       <Route name="reset-password" path="/reset-password/:token" component={ResetPassword} />
+
+       <Public path="/login" component={Login} {...appProps} />
+       <Authenticated path="/" component={MainWrapper} {...appProps} />
     </div>
   </Router>
 );
