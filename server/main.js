dfd84876bb25605eb26e024ff29ba09a67f70611
Mario de la Torret <delatorremario@gmail.com>
2017-10-19 17:35:05 -0300

    add start develpment enviroment

--- server/main.js
+++ server/main.js
@@ -1,6 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import Start from './start';
+import developmentEnv from './env';
 
 Meteor.startup(() => {
   Start.start();
+  if (Meteor.isDevelopment) {
+    console.log('Meteor.isDevelopment');
+    developmentEnv();
+  }
+  if (Meteor.isProduction) console.log('Meteor.isProduction');
 });
