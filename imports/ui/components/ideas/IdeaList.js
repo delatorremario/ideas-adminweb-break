127901c482dd490cadc9e544d902945c0ab545d4
Mario de la Torret <delatorremario@gmail.com>
2017-11-05 17:40:03 -0300

    add card idea simple

--- imports/ui/components/ideas/IdeaList.js
+++ imports/ui/components/ideas/IdeaList.js
@@ -53,25 +53,41 @@ const IdeasList = ({ history, ideas }) => (
             <div className="row cards-container">
                 {ideas.map((idea, index) => {
                     let lap = index / 2;
-
+                    const { person, chief, description, opportunity, collaborators, drivers, origin } = idea;
                     return (
                         <div key={index} className="col-sm-6 col-lg-4 cards-item">
                             <div className="panel panel-default">
                                 <div className="panel-heading">
-                                    <h3 className="panel-title">{idea.origin}</h3>
+                                    <h3 className="panel-title">{idea.person.lastName}</h3>
                                     <div className="actions pull-right">
                                         <Link to={`/idea/${idea._id}/edit`}><i className="fa fa-pencil"></i></Link>
                                         <i className="fa fa-trash" onClick={() => { handleRemove(history, idea._id) }}></i>
                                     </div>
                                 </div>
-                                {/* <div className="row panel-body">
-                                    <p className="col-md-12 panel-body-title"><i className="fa fa-user"></i><b>Administradores</b></p>
+                                <div className="row panel-body">
+                                    <p className="col-md-12 panel-body-title">
+                                        <i className="fa fa-user"></i> Idea de <b>{person.firstName} {person.secondName} {person.lastName}</b>
+                                    </p>
                                     <div className="col-md-12 panel-body-description">
-                                        <ul className="panel-list">
-                                            {idea.adminsEmails.map((email, index) => (<li key={index}> {email} </li>))}
-                                        </ul>
+
+                                        <p>Encargado de Area: <b>{chief.firstName} {chief.secondName} {chief.lastName}</b></p>
+                                        <p>Descripción: <b>{description}</b></p>
+                                        <p>Oportunidad: <b>{opportunity}</b></p>
+                                        <p>Origin: <b>{origin}</b></p>
+                                        {
+                                            collaborators && <div>
+                                                <p>Colaboradores</p>
+                                                <ul>
+                                                    {
+                                                        _.map(collaborators, (person, index) =>
+                                                            <li key={index}>{person.firstName} {person.secondName} {person.lastName}</li>
+                                                        )
+                                                    }
+                                                </ul>
+                                            </div>
+                                        }
                                     </div>
-                                </div> */}
+                                </div>
                             </div>
                         </div>
                     )
