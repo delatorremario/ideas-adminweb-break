255fb744385d2e306844e717f7ebae821cc08ec7
Mario de la Torret <delatorremario@gmail.com>
2017-11-27 12:43:20 -0300

    add temporal alerts scaffolding

--- imports/ui/components/configs/ConfigEditorAlert.js
+++ imports/ui/components/configs/ConfigEditorAlert.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap'
 
 import Switch from 'react-toggle-switch'
 
@@ -8,18 +9,34 @@ class ConfigEditorAlert extends Component {
     state = {
         temporal: false,
         stateChange: false,
+        delay: 1,
+        daily: false,
+        weekly: false,
+    }
 
+    onChange = e => {
+        this.setState(
+            { ...this.state, [e.target.name]: e.target.value }
+        );
     }
 
     toggleTemporalSwitch = () => {
         this.setState((prev) => ({ temporal: !prev.temporal }))
     }
+
     toggleStateChangeSwitch = () => {
         this.setState((prev) => ({ stateChange: !prev.stateChange }))
     }
 
+    toggleDailySwitch = () => {
+        this.setState((prev) => ({ daily: !prev.daily }))
+    }
+    toggleWeeklySwitch = () => {
+        this.setState((prev) => ({ weekly: !prev.weekly }))
+    }
+
     render() {
-        const { temporal, stateChange } = this.state;
+        const { temporal, stateChange, delay, daily, weekly } = this.state;
         return (
             <div className="config-editor-alert">
                 <div className="alert-types">
@@ -32,6 +49,36 @@ class ConfigEditorAlert extends Component {
                         <Switch onClick={this.toggleTemporalSwitch.bind(this)} on={temporal} />
                     </div>
                 </div>
+                {
+                    temporal && <div className="temporal">
+                        <div className="alert-types">
+                            <div className="alert-type">
+                                <div className="alert-type-text">alerta diaria</div>
+                                <Switch onClick={this.toggleDailySwitch.bind(this)} on={daily} />
+                            </div>
+                            <div className="alert-type">
+                                <div className="alert-type-text">alerta semanal</div>
+                                <Switch onClick={this.toggleWeeklySwitch.bind(this)} on={weekly} />
+                            </div>
+                        </div>
+                        <div className="config-times">
+                            <label>Días de Atraso</label>
+                            <input
+                                className="form-control input-sm"
+                                min="1" step="1"
+                                type="number"
+                                name="delay"
+                                value={delay}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                    </div>
+                }
+
+                <div className="config-sends"></div>
+                <div className="config-to"></div>
+                <div className="config-message"></div>
+
             </div>
         )
     }
