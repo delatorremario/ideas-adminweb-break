0a5adfc27801e015ec5ba2a9e0b2d510c3703860
chetinchog <chetinchog@yahoo.com>
2017-11-29 11:34:00 -0300

    Estilo Step2

--- imports/ui/components/ideas/IdeasStep2.js
+++ imports/ui/components/ideas/IdeasStep2.js
@@ -8,17 +8,17 @@ import StateCard from './StateCard';
 const IdeasStep2 = ({ onChangeForm, data, onChangeSearchPerson, persons, selectChief, selectState, ideasstates }) =>
 
     (
-        <div className="row form-steps step-two">
+        <div className="row panel-body form-steps step-two">
             <div className="col-xs-12">
-                <h2>Selección de Encargado de Área de Destino</h2>
+                <h2 className="stepH2">Selección de Encargado de Área de Destino</h2>
                 <div className="row" style={{ display: "flex" }}>
                     {
-                        data && data.chief && <PersonCard person={data.chief} removePerson={selectChief} />
+                        data && data.chief && <PersonCard className="personCard" person={data.chief} removePerson={selectChief} />
                     }
                 </div>
                 <div className="row">
                     {
-                        data && !data.chief && <PersonSearch persons={persons} onChangeSearchPerson={onChangeSearchPerson} selectPerson={selectChief} />
+                        data && !data.chief && <PersonSearch className="personSearch" persons={persons} onChangeSearchPerson={onChangeSearchPerson} selectPerson={selectChief} />
                     }
                 </div>
             </div>
