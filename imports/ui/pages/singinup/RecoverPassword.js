1b9624caffabbc1c3bcf80d574314ecdcdbdfb3c
chetinchog <chetinchog@yahoo.com>
2017-12-05 10:57:23 -0300

    Estilo en recuperar contraseña y crear cuenta

--- imports/ui/pages/singinup/RecoverPassword.js
+++ imports/ui/pages/singinup/RecoverPassword.js
@@ -26,30 +26,30 @@ export default class RecoverPassword extends React.Component {
             <h1 className="page-header">Recuperar Contraseña</h1>
           </div>
           <hr />
-              <Alert bsStyle="info">
-              Ingrese su dirección de correo electrónico a continuación para recibir un enlace y restablecer su contraseña.
+          <Alert bsStyle="info">
+            Ingrese su dirección de correo electrónico a continuación para recibir un enlace y restablecer su contraseña.
               </Alert>
 
-              {loading && <Alert bsStyle="warning">Enviando email!</Alert>}
+          {loading && <Alert bsStyle="warning">Enviando email!</Alert>}
 
-              <form
-                ref={form => (this.recoverPasswordForm = form)}
-                className="recover-password"
-                onSubmit={this.handleSubmit}
-              >
-                <FormGroup>
-                  <FormControl
-                    type="email"
-                    ref="emailAddress"
-                    name="emailAddress"
-                    placeholder="Email Address"
-                  />
-                </FormGroup>
-                <Button type="submit" className="btn btn-success btn-lg btn-block">Recuperar Contraseña</Button>
-              </form>
-              <p> <span> <Link to="/login">¿Ya tienes una cuenta? Iniciar sesión.</Link></span></p>
-          
-         </div>
+          <form
+            ref={form => (this.recoverPasswordForm = form)}
+            className="recover-password"
+            onSubmit={this.handleSubmit}
+          >
+            <FormGroup>
+              <FormControl
+                type="email"
+                ref="emailAddress"
+                name="emailAddress"
+                placeholder="Email Address"
+              />
+            </FormGroup>
+            <Button type="submit" className="btn btn-success btn-lg btn-block">Recuperar Contraseña</Button>
+          </form>
+          <p> <span> <Link to="/login">¿Ya tienes una cuenta? Iniciar sesión.</Link></span></p>
+
+        </div>
       </div>
     );
   }
--- imports/ui/pages/singinup/Signup.js
+++ imports/ui/pages/singinup/Signup.js
@@ -21,51 +21,50 @@ export default class Signup extends React.Component {
             <h1 className="page-header">Crear Cuenta</h1>
           </div>
           <hr />
-         
-            <form
-              ref={form => (this.signupForm = form)}
-              onSubmit={this.handleSubmit}
-            >
-              <FormGroup>
-                <ControlLabel>Nombre</ControlLabel>
-                <FormControl
-                  type="text"
-                  ref="firstName"
-                  name="firstName"
-                  placeholder="Nombre"
-                />
-              </FormGroup>
-              <FormGroup>
-                <ControlLabel>Apellido</ControlLabel>
-                <FormControl
-                  type="text"
-                  ref="lastName"
-                  name="lastName"
-                  placeholder="Apellido"
-                />
-              </FormGroup>
-              <FormGroup>
-                <ControlLabel>Email</ControlLabel>
-                <FormControl
-                  type="text"
-                  ref="emailAddress"
-                  name="emailAddress"
-                  placeholder="Email"
-                />
-              </FormGroup>
-              <FormGroup>
-                <ControlLabel>Contraseña</ControlLabel>
-                <FormControl
-                  type="password"
-                  ref="password"
-                  name="password"
-                  placeholder="Contraseña"
-                />
-              </FormGroup>
-              <Button type="submit" className="btn btn-success btn-lg btn-block">Crear Cuenta</Button>
-            </form>
-            <p> <span> <Link to="/login">¿Ya tienes una cuenta? Iniciar sesión.</Link></span></p>
-          
+          <form
+            className="signup"
+            ref={form => (this.signupForm = form)}
+            onSubmit={this.handleSubmit}
+          >
+            <FormGroup>
+              <ControlLabel>Nombre</ControlLabel>
+              <FormControl
+                type="text"
+                ref="firstName"
+                name="firstName"
+                placeholder="Nombre"
+              />
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Apellido</ControlLabel>
+              <FormControl
+                type="text"
+                ref="lastName"
+                name="lastName"
+                placeholder="Apellido"
+              />
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Email</ControlLabel>
+              <FormControl
+                type="text"
+                ref="emailAddress"
+                name="emailAddress"
+                placeholder="Email"
+              />
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Contraseña</ControlLabel>
+              <FormControl
+                type="password"
+                ref="password"
+                name="password"
+                placeholder="Contraseña"
+              />
+            </FormGroup>
+            <Button type="submit" className="btn btn-success btn-lg btn-block">Crear Cuenta</Button>
+          </form>
+          <p> <span> <Link to="/login">¿Ya tienes una cuenta? Iniciar sesión.</Link></span></p>
         </div>
       </div>
     );
