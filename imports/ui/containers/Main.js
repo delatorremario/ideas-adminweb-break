083e4d747adfbbfb844ac043d9e474252de09c33
Mario de la Torret <delatorremario@gmail.com>
2017-12-19 16:34:42 -0300

    add userimage to main

--- imports/ui/containers/Main.js
+++ imports/ui/containers/Main.js
@@ -2,11 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import { composeWithTracker } from 'react-komposer';
 import MainWrapper from '../layouts/MainWrapper';
 import Loading from '../components/Loading.js';
+import Files from '../../api/files/files';
 
 const composer = ({ match }, onData) => {
     const user = Meteor.user();
-    if (user) {
-        onData(null, { user });
+    const imageId = user && user.profile && user.profile.imageId;
+    const subsFiles = Meteor.subscribe('files.list', [imageId]);
+
+    if (user && subsFiles.ready()) {
+        const userImg = Files.findOne({_id:imageId});
+        onData(null, { user, userImg });
     }
 };
 
