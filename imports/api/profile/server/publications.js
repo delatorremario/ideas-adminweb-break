e334b752ff9bf046fd5e49eb33992fb7685ed406
Mario de la Torret <delatorremario@gmail.com>
2017-12-28 11:56:44 -0300

    fix publish user profile _id

--- imports/api/profile/server/publications.js
+++ imports/api/profile/server/publications.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import _ from 'lodash';
 import { check } from 'meteor/check';
 
-Meteor.publish('user.profile', () => {
-    // check(_id, String);
-    return Meteor.users.find({}, { fields: { profile: 1 } });
+Meteor.publish('user.profile', (_id) => {
+    check(_id, String);
+    return Meteor.users.find({ _id }, { fields: { emails:1, profile: 1 } });
 });
