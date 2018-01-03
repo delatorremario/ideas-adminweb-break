70d77aba52c040fa466ea5ff9be4944641190f43
Martín <chetinchog@yahoo.com>
2018-01-03 10:23:09 -0300

    Categoría de Ideas Comentadas

--- imports/ui/components/comments/ComentsAcordionComponent.js
+++ imports/ui/components/comments/ComentsAcordionComponent.js
@@ -5,9 +5,9 @@ import moment from 'moment';
 import CommentsComponent from './CommentsComponent';
 
 const CommentsAcordionComponent = ({ ideas }) => {
+    let iSV = [];
     let iCC = [];
     let iSC = [];
-    let iSV = [];
     _.forEach(ideas, (idea) => {
         if (idea.comments && idea.comments.length > 0) {
             /* TODO: Filtrar por visto o no visto */
@@ -19,21 +19,43 @@ const CommentsAcordionComponent = ({ ideas }) => {
     return (
         <div>
             <Accordion>
-                {
-                    iCC.length < 1 ? '' :
-                    _.map(iCC, (idea, index) => {
-                        const cantComments = idea && idea.comments && idea.comments.length || 0;
-                        if (cantComments > 0) {
-                            return <Panel className="ci-item" header={idea.opportunity + ' (' + cantComments + ') - ' + moment(idea.createAt).format('DD/MM/YYYY')} eventKey={index} key={index}>
-                                <CommentsComponent idea={idea} />
-                            </Panel>
-                        } else {
-                            return
+                <Panel className="ci-item" header={`Ideas con comentarios no leidos (${iSV.length})`} eventKey="1" key="1">
+                    <Accordion key="1">
+                        {
+                            _.map(iSV, (idea, index) => {
+                                const cantComments = idea && idea.comments && idea.comments.length || 0;
+                                return <Panel className="ci-item" header={idea.opportunity + ' (' + cantComments + ') - ' + moment(idea.createAt).format('DD/MM/YYYY')} eventKey={index} key={index}>
+                                    <CommentsComponent idea={idea} />
+                                </Panel>
+                            })
                         }
-                    })
-                }
+                    </Accordion>
+                </Panel>
+                <Panel className="ci-item" header={`Ideas comentadas (${iCC.length})`} eventKey="2" key="2">
+                    <Accordion key="2">
+                        {
+                            _.map(iCC, (idea, index) => {
+                                const cantComments = idea && idea.comments && idea.comments.length || 0;
+                                return <Panel className="ci-item" header={idea.opportunity + ' (' + cantComments + ') - ' + moment(idea.createAt).format('DD/MM/YYYY')} eventKey={index} key={index}>
+                                    <CommentsComponent idea={idea} />
+                                </Panel>
+                            })
+                        }
+                    </Accordion>
+                </Panel>
+                <Panel className="ci-item" header={`Ideas sin comentarios (${iSC.length})`} eventKey="3" key="3">
+                    <Accordion key="3">
+                        {
+                            _.map(iSC, (idea, index) => {
+                                const cantComments = idea && idea.comments && idea.comments.length || 0;
+                                return <Panel className="ci-item" header={idea.opportunity + ' (' + cantComments + ') - ' + moment(idea.createAt).format('DD/MM/YYYY')} eventKey={index} key={index}>
+                                    <CommentsComponent idea={idea} />
+                                </Panel>
+                            })
+                        }
+                    </Accordion>
+                </Panel>
             </Accordion>
-            {/* TODO: Agregar lista de ideas sin comentar */}
         </div>
     )
 }
