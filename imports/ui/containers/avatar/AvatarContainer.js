6f117da83ef31ccde7febd2f8e6c3ee1f97d3d25
Martín <chetinchog@yahoo.com>
2018-01-05 12:27:50 -0300

    Estilo en Avatar

--- imports/ui/containers/avatar/AvatarContainer.js
+++ imports/ui/containers/avatar/AvatarContainer.js
@@ -4,10 +4,12 @@ import Loading from '../../components/Loading.js';
 import AvatarComponent from '../../components/avatar/AvatarComponent';
 
 const composer = ({ }, onData) => {
+    let user = Meteor.user();
+    console.log(user);
     let userImg;
-    let color = 'red';
-    let name = 'Mario de la Torre';
-    let email = 'mdelatorre@holos.cl';
+    let color = (user.profile && user.profile.color) || '#337ab7';
+    let name = (user.profile && (user.profile.firstName + ' ' + user.profile.lastName)) || 'O';
+    let email = user.emails[0].address;
     onData(null, { userImg, color, name, email });
 };
 
