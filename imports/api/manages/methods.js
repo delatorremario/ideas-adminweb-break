e19a5230f3572b13d0be27e89c304f4d9f391db0
Mario de la Torret <delatorremario@gmail.com>
2018-01-08 14:50:53 -0300

    fix to mis areas leaders.update

--- imports/api/manages/methods.js
+++ imports/api/manages/methods.js
@@ -2,13 +2,14 @@ import { Meteor } from 'meteor/meteor';
 
 import States from '../../api/states/states';
 import Ideas from '../../api/ideas/ideas';
+import Areas from '../../api/areas/areas';
 
 Meteor.methods({
     'manages.states': () => {
         // role this user
         if (!Meteor.isServer) return;
 
-        const { roles } = Meteor.user();
+        const { roles, profile } = Meteor.user();
         const states = States.aggregate([
             { $unwind: '$roles' },
             { $match: { 'roles.role': { $in: roles } } },
@@ -16,16 +17,24 @@ Meteor.methods({
             { $project: { _id: 0, title: '$_id', codes: 1 } },
         ]);
 
+        const filters = {}
+
+        const area = Areas.findOne({ _id: profile && profile.leaderAreaId })
+        _.extend(filters, {
+            $or: [
+                { 'person._id': profile && profile._id },
+                { 'chief.areaId': { $in: area && area.family } }
+            ]
+        })
+
         _.map(states, state => {
 
             const codesFilter = [];
             _.each(state.codes, code => {
                 codesFilter.push({ $where: `this.states[this.states.length - 1].code === '${code}'` })
             })
-
-            const ideas = Ideas.find({
-                $or: codesFilter
-            }).fetch();
+            _.extend(filters, { $or: codesFilter })
+            const ideas = Ideas.find(filters).fetch();
             _.extend(state, { ideas, count: ideas.length });
             return state;
         })
--- server/upsertLeaders.js
+++ server/upsertLeaders.js
@@ -11,25 +11,24 @@ Meteor.methods({
         if (!Meteor.isServer) return;
 
         const leadersMails = [
-            { mail: 'Jorge.JM.Jalil@bhpbilliton.com', area: 'Cathode Operations' },
-            { mail: 'Jorge.JN.Tom@bhpbilliton.com', area: 'Mine Operations' },
-            { mail: 'Camila.CA.Perez@bhpbilliton.com', area: 'Concentrate Operations' },
-            { mail: 'Mauricio.MA.Rodriguez@bhpbilliton.com', area: 'NPI & Conc. Handling Operations' },
-            { mail: 'Maria.MP.Bustamante@bhpbilliton.com', area: 'Logistic & Execution Integ Operations' },
+            { mail: 'Jorge.JM.Jalil@bhpbilliton.com', areas: ['Cathode Operations'] },
+            { mail: 'Jorge.JN.Tom@bhpbilliton.com', areas: ['Mine Operations'] },
+            { mail: 'Camila.CA.Perez@bhpbilliton.com', areas: ['Concentrate Operations'] },
+            { mail: 'Mauricio.MA.Rodriguez@bhpbilliton.com', areas: ['NPI & Conc. Handling Operations'] },
+            { mail: 'Maria.MP.Bustamante@bhpbilliton.com', areas: ['Logistic & Execution Integ Operations'] },
         ]
 
         _.each(leadersMails, leader => {
             const email = _.toLower(leader.mail);
-
-            const area = Areas.findOne({ name: leader.area });
-            if (!area) { console.log('-- NO EXISTE AREA --', leader.area); return; }
-            const user = Meteor.users.findOne({ 'emails.address': email });
-            if (!user) { console.log('-- NO EXISTE USUARIO --', email); return; }
-
-            Roles.addUsersToRoles(user._id, 'Leader');
-            Meteor.users.update({ _id: user._id }, { $set: { 'profile.leaderAreaId': area._id } });
-
-            console.log('add Leader', email);
+            _.each(leader.areas, area =>{
+                const myarea = Areas.findOne({ name: area });
+                if (!myarea) { console.log('-- NO EXISTE AREA --', area); return; }
+                const user = Meteor.users.findOne({ 'emails.address': email });
+                if (!user) { console.log('-- NO EXISTE USUARIO --', email); return; }
+                Roles.addUsersToRoles(user._id, 'Leader');
+                Meteor.users.update({ _id: user._id }, { $addToSet: { 'profile.leaderAreasIds': myarea._id } });
+                console.log('add Leader', email);
+            })
         })
 
 
