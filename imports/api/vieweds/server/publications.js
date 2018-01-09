04326ecabc749f7197be311e2148f72346d52795
Martín <chetinchog@yahoo.com>
2018-01-09 18:30:55 -0300

    Console.log

--- imports/api/vieweds/server/publications.js
+++ imports/api/vieweds/server/publications.js
@@ -5,13 +5,11 @@ import Vieweds from '../vieweds';
 
 Meteor.publish('viewed.byUserId', (userId) => {
     check(userId, String);
-    console.log('publish viewed.byIdea');
     return Vieweds.find({ userId: userId }, { sort: { createdAt: 1 } });
 });
 
 Meteor.publish('viewed.byCommentId', (commentId) => {
     check(commentId, String);
-    console.log('publish viewed.byId');
     return Vieweds.find({ commentId: commentId }, { sort: { createdAt: 1 } });
 });
 
