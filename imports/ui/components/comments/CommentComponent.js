b7a88a91aa7782945a45f8cc7d1aafa32fac0b09
Martín <chetinchog@yahoo.com>
2018-01-10 12:56:24 -0300

    Scroll in comments

--- imports/ui/components/comments/CommentComponent.js
+++ imports/ui/components/comments/CommentComponent.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Loading from '../../components/Loading';
 import _ from 'lodash';
 import moment from 'moment';
 import AvatarContainer from '../../containers/avatar/AvatarContainer';
 
-const CommentComponent = ({ text, name, userId, date, color, index }) => {
-    return <div className="ci-comment">
-        <div className="ci-comment-info">
-            <div className="ci-comment-user">
-                <AvatarContainer userId={userId} />
-                <div>
-                    <div className="ci-comment-name">
-                        <b style={{ color: color }}>{name}</b>
-                    </div>
-                    <div className="ci-comment-date">
-                        <i>
-                            {
-                                moment(date).format('HH:mm DD/MM/YYYY')
-                            }
-                        </i>
+export default class CommentComponent extends Component {
+
+    componentDidMount() {
+        $('html,body').stop(true, true);
+        $('html,body').animate({ scrollTop: document.body.scrollHeight }, 0, "swing");
+    }
+
+    render() {
+        const { text, name, userId, date, color, index } = this.props;
+        return <div className="ci-comment">
+            <div className="ci-comment-info">
+                <div className="ci-comment-user">
+                    <AvatarContainer userId={userId} />
+                    <div>
+                        <div className="ci-comment-name">
+                            <b style={{ color: color }}>{name}</b>
+                        </div>
+                        <div className="ci-comment-date">
+                            <i>
+                                {
+                                    moment(date).format('HH:mm DD/MM/YYYY')
+                                }
+                            </i>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div className="ci-comment-text">
+                {text}
+            </div>
         </div>
-        <div className="ci-comment-text">
-            {text}
-        </div>
-    </div>
-}
+    }
 
-export default CommentComponent;
+}
--- imports/ui/pages/comments/IdeaCommentsPage.js
+++ imports/ui/pages/comments/IdeaCommentsPage.js
@@ -1,47 +1,49 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router-dom';
 import NotFound from '../NotFound';
 import CommentsComponent from '../../components/comments/CommentsComponent';
 import moment from 'moment';
+import UpSideDown from '../../components/upsidedown/UpSideDown';
 
-const IdeaCommentsPage = (props) => {
-    let { idea } = props;
-    return (
-        <div>
-            <div className="pageheader">
-                <h1>
-                    Comentarios
+export default class IdeaCommentsPage extends Component {
+    render() {
+        const { idea } = this.props;
+        return (
+            <div>
+                <div className="pageheader">
+                    <h1>
+                        Comentarios
                 </h1>
-                <p>
-                    <br />
-                    <b>{idea.opportunity ? idea.opportunity : 'de una idea...'}</b>
-                </p>
-                <div className="breadcrumb-wrapper hidden-xs">
-                    <span className="label">You are here:</span>
-                    <ol className="breadcrumb">
-                        <li className="active">
-                            Comentarios de {
-                                idea.opportunity ? idea.opportunity : 'una idea...'
-                            }
-                        </li>
-                    </ol>
+                    <p>
+                        <br />
+                        <b>{idea.opportunity ? idea.opportunity : 'de una idea...'}</b>
+                    </p>
+                    <div className="breadcrumb-wrapper hidden-xs">
+                        <span className="label">You are here:</span>
+                        <ol className="breadcrumb">
+                            <li className="active">
+                                Comentarios de {
+                                    idea.opportunity ? idea.opportunity : 'una idea...'
+                                }
+                            </li>
+                        </ol>
+                    </div>
                 </div>
-            </div>
-            <section id="main-content">
-                <div className="row">
-                    <div className="col-md-12 col-lg-12 noneMargin">
-                        <div className="panel">
-                            <div className="panel-body ng-binding">
-                                <div className="ci-item">
-                                    <CommentsComponent idea={idea} />
+                <section id="main-content">
+                    <div className="row">
+                        <div className="col-md-12 col-lg-12 noneMargin">
+                            <div className="panel">
+                                <div className="panel-body ng-binding">
+                                    <div className="ci-item">
+                                        <CommentsComponent idea={idea} />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        </div>
-    )
-};
-
-export default IdeaCommentsPage;
+                </section>
+                <UpSideDown />
+            </div>
+        )
+    };
+}
