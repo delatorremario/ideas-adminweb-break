cad2302973d8df574d285031d92a494e2abce710
Mario de la Torret <delatorremario@gmail.com>
2018-01-19 20:44:01 -0300

    fix upsert persons

--- server/upsertPersons.js
+++ server/upsertPersons.js
@@ -15,27 +15,29 @@ Meteor.methods({
 
         /* add typesareasstructure */
         _.map(corporations, (corp) => {
-            _.map(masterPersons, person => {
+            _.map(masterPersons, (person, index) => {
 
                 _.extend(person, { corporationId: corp._id, origin: 'MEL' });
-
+                //console.log('index', index);
                 /* set area */
                 const area = person.areaCode && Areas.findOne({ code: person.areaCode, corporationId: corp._id });
                 if (!area) area = Areas.findOne({ name: 'Escondida' });
-                // if (person.email != 'neftali.a.herrera@bhpbilliton.com') return;
-                // console.log('--email--', person.email, area.name);
+                //console.log('--email--', person.email, area.name);
+                //if (person.email != 'neftali.a.herrera@bhpbilliton.com') return;
                 /* end set area */
 
                 const find = Persons.findOne({ rut: person.rut });
                 if (area) _.extend(person, { areaId: area._id, area: area.name });
                 else console.log('sin area');
-
+                person = _.merge(find, person);
                 Persons.upsert({ _id: find && find._id || '' }, { $set: { ...person } }, (err, data) => {
                     console.info('UPSERT', data, person.email, err);
                     Meteor.users.update({ 'emails.address': person.email }, { $set: { profile: { ...person } } })
                 });
             })
         })
-        console.log('**** persons.populate call END ****')
+        const message = '**** persons.populate call END ****'
+        console.log(message)
+        return message
     }
 });
