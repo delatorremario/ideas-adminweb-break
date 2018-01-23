133afe21cff30480ac54c283c1c4ae45da5fb492
Mario de la Torret <delatorremario@gmail.com>
2018-01-23 17:41:44 -0300

    fix email to lower case

--- imports/startup/server/accounts/accounts.js
+++ imports/startup/server/accounts/accounts.js
@@ -10,13 +10,17 @@ import Corporations from '../../../api/corporations/corporations';
 Accounts.onLogin((data) => {
     const user = data.user;
 
-    if (user && !Roles.userIsInRole(user, ['SuperAdminHolos', 'AdminGrupoNegocio', 'Leader', 'Employee', 'Executive', 'Reporter'])) {
-        Roles.addUsersToRoles(user, ['Employee']);
-    }
+  
 
     const email = user.emails[0].address
 
     const person = Persons.findOne({ email });
+
+    if(person && person.group==='EXECUT.') Roles.addUsersToRoles(user, ['Executive']);
+    
+    if (user && !Roles.userIsInRole(user, ['SuperAdminHolos', 'AdminGrupoNegocio', 'Leader', 'Employee', 'Executive', 'Reporter'])) {
+        Roles.addUsersToRoles(user, ['Employee']);
+    }
 
     if (!user.profile) _.extend(user, { profile: {} })
     if (!user.profile.corporationId) {
