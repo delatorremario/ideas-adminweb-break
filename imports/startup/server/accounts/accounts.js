cf2b6db9fd051b2d12d2eed7152d26dfddfbc713
Mario de la Torret <delatorremario@gmail.com>
2018-01-25 11:24:18 -0300

    fix profile login

--- imports/startup/server/accounts/accounts.js
+++ imports/startup/server/accounts/accounts.js
@@ -10,27 +10,26 @@ import Corporations from '../../../api/corporations/corporations';
 Accounts.onLogin((data) => {
     const user = data.user;
 
-  
-
-    const email = user.emails[0].address
-
-    const person = Persons.findOne({ email });
-
-    if(person && person.group==='EXECUT.') Roles.addUsersToRoles(user, ['Executive']);
-    
-    if (user && !Roles.userIsInRole(user, ['SuperAdminHolos', 'AdminGrupoNegocio', 'Leader', 'Employee', 'Executive', 'Reporter'])) {
-        Roles.addUsersToRoles(user, ['Employee']);
-    }
-
     if (!user.profile) _.extend(user, { profile: {} })
     if (!user.profile.corporationId) {
         const corp = Corporations.findOne();
         Meteor.users.update({ _id: user._id }, { $set: { 'profile.corporationId': corp && corp._id } })
     }
+
+    const email = user.emails[0].address
+
+    const person = Persons.findOne({ email });
+
+    if (person) {
+        if (person && person.group === 'EXECUT.') Roles.addUsersToRoles(user, ['Executive']);
+        if (person && person.origin === 'MEL') Meteor.users.update({ _id: user._id }, { $set: { profile: { ...person } } })
+
+    }
+
     if (!person) {
-        const area = Areas.findOne();
-        const areaId = area && area._id;
-        _.extend(user.profile, { areaId, email })
+        // const area = Areas.findOne();
+        // const areaId = area && area._id;
+        // _.extend(user.profile, { areaId, email })
         console.log('--- no existe en persons ---');
         // si el usuario no existe en PERSONS lo agrega
 
@@ -38,6 +37,13 @@ Accounts.onLogin((data) => {
             console.log('upsert person', err, data);
         })
     }
+
+    if (user && !Roles.userIsInRole(user, ['SuperAdminHolos', 'AdminGrupoNegocio', 'Leader', 'Employee', 'Executive', 'Reporter'])) {
+        Roles.addUsersToRoles(user, ['Employee']);
+    }
+
+
+
     console.log('Login complete!');
 
 
