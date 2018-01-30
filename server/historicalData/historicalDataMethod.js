02b5929ba9832d0ed1d7414b08f903584237f1d5
Mario de la Torret <delatorremario@gmail.com>
2018-01-30 15:04:27 -0300

    add historicalData

---
+++ server/historicalData/historicalDataMethod.js
@@ -0,0 +1,76 @@
+import { Meteor } from 'meteor/meteor';
+import _ from 'lodash';
+import Ideas from '../../imports/api/ideas/ideas';
+import Persons from '../../imports/api/persons/persons';
+import States from '../../imports/api/states/states';
+import Areas from '../../imports/api/areas/areas';
+import { findChiefOne } from '../../imports/api/areas/methods';
+
+import historicalDataDb from './historicalDataDb';
+
+Meteor.methods({
+    'import.historical': () => {
+        const errors = [];
+        let updated = 0
+        let inserted = 0
+        _.each(historicalDataDb, (data, index) => {
+            const { date, origin, personRef, stateCode, impactId, stateDate, areaCode, opportunity, description, drivers } = data;
+            let person = Persons.findOne({
+                $or: [{ masterCode: personRef }, { email: personRef }, { rut: personRef }]
+            });
+            const state = States.findOne({ code: stateCode });
+            state.createdAt = new Date()
+            state.updatedAt = new Date()
+            const area = Areas.findOne({ code: areaCode });
+
+            const chief = area && findChiefOne(area);
+
+            if (!area) { errors.push({ err: 'no existe area', index, areaCode }); return; }
+            if (!person) {
+                if (validateEmail(personRef)) {
+                    const idPerson = Persons.insert({ email: personRef, areaId: area._id })
+                    console.log('idPerson', idPerson);
+
+                    person = Persons.findOne(idPerson);
+                }
+                else
+                    errors.push({ err: 'no existe persona', index, personRef }); return;
+            }
+            if (!state) { errors.push({ err: 'no existe state', index, stateCode }); return; }
+            if (!chief) { _.extend(chief, { areaId: area._id }); errors.push({ warning: 'no existe chief', index, areaCode }); }
+
+            const idea = {
+                date: new Date(date),
+                origin,
+                person,
+                chief,
+                description,
+                opportunity,
+                drivers,
+                states: [state],
+                impactId
+            }
+
+            Ideas.upsert({
+                description,
+                opportunity,
+            }, { $set: { ...idea } }, (err, data) => {
+                if (err) { errors.push({ err: err.message, index }); return; }
+                const { insertedId } = data;
+                if (insertedId) { inserted++; Meteor.call('idea.addViewers', insertedId) }
+                else updated++
+                console.log('upsert', data);
+            })
+
+        })
+        const result = { inserted, updated, total: inserted + updated, from: historicalDataDb.length, errors }
+        console.log('result', result);
+        return result
+    }
+})
+
+
+const validateEmail = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
